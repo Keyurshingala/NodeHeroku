818c297a38b23026447429b8d12d09ad9eb39665
Keyurshingala <shingala.keyur@gmail.com>
2022-11-15 20:51:06 +0530

    3rd

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 import express from "express";
 import path from "path";
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 var app = express()
 
-app.get("/", async (req, res) => {
+app.get("/",   (req, res) => {
     res.send('<h1> Hellow </h1>');
     res.end()
 })
 
-app.get("/file", async (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.htmll'))
+app.get("/file",   (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get("/search", async (req, res) => {
+app.get("/search",   (req, res) => {
     res.sendFile(path.resolve('data.json'));
-
 });
 
 const PORT = process.env.PORT || 5000;
